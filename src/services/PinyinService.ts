24bd0c32139e78a4c6483f4d9e8702c98d63034d
Elyse <kevincristian@outlook.com>
2022-01-30 21:36:59 -0600

    Remove unnecessary npm dependencies

--- src/services/PinyinService.ts
+++
@@ -1,9 +0,0 @@
-import { HanziTools, pinyinify } from 'hanzi-tools';
-
-class PinyinService {
-  public static getPinyinText(chineseSimplifiedText: string): string | HanziTools.PinyinDetails {
-    return pinyinify(chineseSimplifiedText, true);
-  }
-}
-
-export default PinyinService;
