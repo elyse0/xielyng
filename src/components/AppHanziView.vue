99589df2ed945a6df20094ef8e0a73fffbffe0b7
Elyse <kevincristian@outlook.com>
2022-02-04 22:39:28 -0600

    Split hanzi text if it contains more than one character
    Previously we were failing to display hanzi texts with
    more than one character because HanziWriter only supports
    a single character at a time.

--- src/components/AppHanziView.vue
+++ src/components/AppHanziView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hanzi-view has-text-centered">
-    <div :id="uuid"></div>
+  <div class="hanzi-view">
+    <div class="item" v-for="item in hanziItems" :key="item.id" :id="item.id"/>
   </div>
 </template>
 
@@ -15,17 +15,33 @@ interface Props {
 }
 
 const props = defineProps<Props>();
-const uuid = getOnlyLettersUuid();
+
+const hanziItems = Array.from(props.hanzi)
+  .map((item) => ({
+    id: getOnlyLettersUuid(),
+    hanzi: item,
+  }));
+
+// FIXME: Find a better way to compute the scale when there are more than two hanzi symbols
+const scale = (-0.2 * hanziItems.length * hanziItems.length) + (0.6 * hanziItems.length) + 0.6;
 
 onMounted(() => {
-  HanziWriter.create(uuid, props.hanzi, {
-    width: 170,
-    height: 170,
-    padding: 5,
-    radicalColor: '#168F16',
+  hanziItems.forEach((item) => {
+    HanziWriter.create(item.id, item.hanzi, {
+      width: 170,
+      height: 170,
+      padding: 5,
+      radicalColor: '#168F16',
+    });
   });
 });
 </script>
 
 <style scoped>
+div.hanzi-view {
+  display: flex;
+  justify-content: center;
+  width: 300px;
+  scale: v-bind(scale);
+}
 </style>
