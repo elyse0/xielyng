197b07a228497c960b933cab4441450d465f10d1
Elyse <kevincristian@outlook.com>
2022-02-12 02:50:11 -0600

    Add AppImage component
    This component uses the lazy-load package that allow us to only
    load images when we need them.

---
+++ src/components/ui/AppImage.vue
@@ -0,0 +1,22 @@
+<template>
+  <img v-lazy="props.src" :alt="alt">
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
