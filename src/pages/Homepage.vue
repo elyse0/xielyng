b5c33e8121c88d3430b174d2366e2ca4f363a513
Elyse <kevincristian@outlook.com>
2022-02-12 02:55:54 -0600

    Use the AppLayout component and add quote to the mainpage

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="homepage">
-
-    <div v-for="(vocabularyItem) in vocabulary" :key="vocabularyItem.hanzi">
-      <AppVocabularyCard :vocabulary-item="vocabularyItem"/>
+  <AppLayout>
+    <div class="quote">
+      <div class="hanzi">
+        <o-tooltip label="Qiān lǐ zhī xíng, shǐ yú zú xià." >
+          千里之行,始于足下
+        </o-tooltip>
+      </div>
+      <div class="translation">
+        A journey of a thousand miles begins with a single step.
+      </div>
+      <div class="author">
+        Lao Tzu
+      </div>
     </div>
-
-  </div>
+  </AppLayout>
 </template>
 
 <script setup lang="ts">
-import vocabulary from '@/vocabulary';
 
-import AppVocabularyCard from '@/components/AppVocabularyCard.vue';
+import AppLayout from '@/components/layout/AppLayout.vue';
 </script>
 
 <style scoped>
+div.quote {
+  text-align: center;
+}
 
+div.quote > div.hanzi {
+  font-size: 45pt;
+  color: #d0bba3;
+}
+
+div.quote > div.translation {
+  font-size: 20pt;
+}
+
+div.quote > div.author {
+  font-size: 20pt;
+}
 </style>
