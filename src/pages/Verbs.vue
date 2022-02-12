7016c20c44e2623ac42e7d99891dcbb4d0f9b026
Elyse <kevincristian@outlook.com>
2022-02-12 02:53:11 -0600

    Use the AppLayout component in Verbs page

--- src/pages/Verbs.vue
+++ src/pages/Verbs.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="verbs">
+  <AppLayout>
+    <div class="verbs">
 
-    <div v-for="(verb, index) in verbs" :key="index">
-      <AppVocabularyCard :vocabulary-item="verb"/>
+      <div v-for="(verb, index) in verbs" :key="index">
+        <AppVocabularyCard :vocabulary-item="verb"/>
+      </div>
+
     </div>
-
-  </div>
+  </AppLayout>
 </template>
 
 <script setup lang="ts">
+import AppLayout from '@/components/layout/AppLayout.vue';
 import verbs from '@/vocabulary/verbs';
 
 import AppVocabularyCard from '@/components/AppVocabularyCard.vue';
