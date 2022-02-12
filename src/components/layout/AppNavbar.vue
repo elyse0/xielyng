a3e4f3f05e7ef2c1dcaa523bbdb1dc00a781f457
Elyse <kevincristian@outlook.com>
2022-02-12 02:50:44 -0600

    Add basic links to AppNavbar

--- src/components/layout/AppNavbar.vue
+++ src/components/layout/AppNavbar.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="navbar">
-
+  <div class="app-navbar">
+    <div class="logo">
+      <router-link to="/">
+        <img src="https://xielyng.s3.fr-par.scw.cloud/logo.png" alt="Xielyng logo" width="50">
+      </router-link>
+    </div>
+    <div class="links">
+      <o-button tag="router-link" :to="{path: '/verbs'}">Verbs</o-button>
+      <o-button tag="router-link" :to="{path: '/videos'}">Videos</o-button>
+      <o-button tag="router-link" :to="{path: '/vocabulary'}">Vocabulary</o-button>
+    </div>
   </div>
 </template>
 
@@ -9,5 +18,26 @@
 </script>
 
 <style scoped>
+div.app-navbar {
+  display: flex;
+  vertical-align: center;
+  margin: 0 0 10px 0;
+}
 
+div.logo {
+  margin: 0 20px 0 10px;
+}
+
+div.links {
+  display: flex;
+  align-items: center;
+}
+
+a.button {
+  background-color: transparent;
+  border-color: dimgray;
+  color: white;
+  font-weight: bold;
+  margin: 0 5px;
+}
 </style>
