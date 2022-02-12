85a0f99235c54c59e012e961c52c7e06597bcb39
Elyse <kevincristian@outlook.com>
2022-02-12 02:51:42 -0600

    Add AppLayout component
    This component now will be the base layout used on all pages.

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-layout">
+    <AppNavbar></AppNavbar>
+    <div class="app-content">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import AppNavbar from '@/components/layout/AppNavbar.vue';
+</script>
+
+<style scoped>
+div.app-content {
+  padding: 5px;
+}
+</style>
