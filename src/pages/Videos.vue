7ff71306f67ca405b8d756491f51e33533ce6afc
Elyse <kevincristian@outlook.com>
2022-02-12 02:53:39 -0600

    Use AppLayout and AppImage component in Videos page

--- src/pages/Videos.vue
+++ src/pages/Videos.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="videos">
+  <AppLayout>
+    <div class="videos">
 
-    <div class="youtube-videos">
-      <div v-for="(video) in youtubeVideosArray" :key="video.id">
-        <o-button tag="router-link"
-                  :to="`/videos/youtube/${video.id}`"
-                  type="is-link">
-          {{ video.title }}
-        </o-button>
+      <div class="youtube-videos">
+        <div class="video" v-for="(video) in youtubeVideosArray" :key="video.id">
+          <router-link :to="`/videos/youtube/${video.id}`">
+            <AppImage :src="video.thumbnailUrl" :alt="video.title"></AppImage>
+          </router-link>
+        </div>
       </div>
-    </div>
 
-  </div>
+    </div>
+  </AppLayout>
 </template>
 
 <script setup lang="ts">
+import AppLayout from '@/components/layout/AppLayout.vue';
+import AppImage from '@/components/ui/AppImage.vue';
 import YoutubeVideos from '@/youtube-videos';
 
 const youtubeVideosArray = Object.keys(YoutubeVideos)
@@ -22,5 +24,13 @@ const youtubeVideosArray = Object.keys(YoutubeVideos)
 </script>
 
 <style scoped>
+div.youtube-videos {
+  display: flex;
+  flex-basis: fit-content;
+  justify-content: space-evenly;
+}
 
+div.youtube-videos > div.video {
+
+}
 </style>
