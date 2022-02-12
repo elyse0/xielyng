f34362be90571d58e7283f21a41b02b06ffc877a
Elyse <kevincristian@outlook.com>
2022-02-12 02:54:37 -0600

    Add optional thumbnailUrl to youtube videos list

--- src/types/youtube-videos.ts
+++ src/types/youtube-videos.ts
@@ -11,4 +11,5 @@ export interface Caption {
 export interface YoutubeVideo {
   title: string,
   captions: Caption[]
+  thumbnailUrl?: string,
 }
--- src/youtube-videos/index.ts
+++ src/youtube-videos/index.ts
@@ -6,10 +6,12 @@ import GNK7rPdabu8 from './subtitles-GNK7rPdabu8.json';
 const youtubeVideos: Record<string, YoutubeVideo> = {
   jmoe4Fz0sBY: {
     title: '【VLOG in Chinese】Off to the US!',
+    thumbnailUrl: 'http://i3.ytimg.com/vi/jmoe4Fz0sBY/hqdefault.jpg',
     captions: jmoe4Fz0sBY,
   },
   GNK7rPdabu8: {
     title: '【VLOG in Chinese】ROOM TOUR 2021',
+    thumbnailUrl: 'http://i3.ytimg.com/vi/GNK7rPdabu8/hqdefault.jpg',
     captions: GNK7rPdabu8,
   },
 };
