cd40e9a23779483cae39c71da74187ef804192bb
Elyse <kevincristian@outlook.com>
2022-02-27 17:55:05 -0600

    Use sidebar in Vocabulary page and allow filter by category

--- src/pages/Vocabulary.vue
+++ src/pages/Vocabulary.vue
@@ -2,8 +2,12 @@
   <AppLayout>
     <div class="vocabulary">
 
-      <div v-for="(vocabularyItem) in vocabulary" :key="vocabularyItem.hanzi">
-        <AppVocabularyCard :vocabulary-item="vocabularyItem"/>
+      <AppSidebar :items="categories" v-model:selected-item="selectedCategory"></AppSidebar>
+
+      <div class="vocabulary-items">
+        <AppVocabularyCard v-for="(vocabularyItem) in filteredVocabulary"
+                           :key="vocabularyItem.hanzi"
+                           :vocabulary-item="vocabularyItem"/>
       </div>
 
     </div>
@@ -11,17 +15,37 @@
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue';
+
 import AppLayout from '@/components/layout/AppLayout.vue';
 import AppVocabularyCard from '@/components/AppVocabularyCard.vue';
+import AppSidebar from '@/components/ui/AppSidebar.vue';
 
 import vocabulary from '@/vocabulary';
+
+const selectedCategory = ref<string | null>(null);
+
+const categories = (() => {
+  const categoriesSet = new Set();
+  vocabulary.forEach((vocabularyItem) => {
+    categoriesSet.add(vocabularyItem.category);
+  });
+  return Array.from(categoriesSet).sort();
+})();
+
+const filteredVocabulary = computed(() => (selectedCategory.value ? vocabulary.filter((item) => item.category === selectedCategory.value) : vocabulary));
 </script>
 
 <style scoped>
 div.vocabulary {
   display: flex;
-  justify-content: space-evenly;
+}
+
+div.vocabulary-items {
+  display: flex;
+  justify-content: space-between;
   flex-basis: fit-content;
   flex-wrap: wrap;
+  flex-grow: 1;
 }
 </style>
