078a860e8dc88a2c79715c40d95fa9215865d820
Elyse <26639800+elyse0@users.noreply.github.com>
2022-08-02 00:25:09 -0500

    Add youtube caption type definitions package

---
+++ packages/youtube-api/src/types/captions.ts
@@ -0,0 +1,28 @@
+interface Caption {
+    text: string;
+    start: number;
+    end: number;
+}
+
+interface VideoCaptions {
+    languageId: string;
+    captions: Caption[]
+}
+
+interface MergedCaption extends Caption {
+    languageId: string;
+}
+
+interface YoutubeCaptions extends VideoCaptions {
+    videoId: string;
+}
+
+interface MergedVideoCaptions {
+    start: number,
+    end: number,
+    captions: Record<string, string | string[]>
+}
+
+export {
+    Caption, MergedCaption, YoutubeCaptions, MergedVideoCaptions, VideoCaptions,
+};
