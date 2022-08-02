fce013aedf42295f82fb4306d5ef665e19ff3355
Elyse <26639800+elyse0@users.noreply.github.com>
2022-08-02 01:07:39 -0500

    Add missing return value type to CaptionService

--- packages/caption-service/src/CaptionService.ts
+++ packages/caption-service/src/CaptionService.ts
@@ -1,12 +1,16 @@
 import { Err, Ok, Result } from 'ts-results-es'
 
 import { HttpError, HttpService } from '@xielyng/http-service'
+import { VideoCaptions } from '@xielyng/youtube-api'
 
 class CaptionService {
     private static youtubeUrl = 'https://youtube-subtitles-zjlkubqtfa-uc.a.run.app';
 
-    static async getYoutubeCaptions(videoId: string, languageId: string) {
-        return HttpService.get(`${CaptionService.youtubeUrl}/${videoId}/${languageId}`);
+    static async getYoutubeCaptions(
+        videoId: string,
+        languageId: string,
+    ): Promise<Result<VideoCaptions, HttpError>> {
+        return HttpService.get<VideoCaptions>(`${CaptionService.youtubeUrl}/${videoId}/${languageId}`);
     }
 
     static async getAvailableYoutubeCaptions(videoId: string): Promise<Result<string[], HttpError>> {
